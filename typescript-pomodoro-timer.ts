7b4f96bb34b068189eea6ae7bc102cf22a4c732f
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-07-09 17:43:56 +0100

    Padstart () Format MM:SS

--- typescript-pomodoro-timer.ts
+++ typescript-pomodoro-timer.ts
@@ -1,8 +1,11 @@
-//Define timer durations for work and break (0. added for testing).
+//Define timer durations for work and break (0.* added for testing).
 const WORK_DURATION = 0.25; 
 const BREAK_DURATION = 0.5;
 
-//Start timer
+//Using padstart, print time in the format: MM:SS.
+function formatTime(minutes: number, seconds: number): string {
+  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
 
 //Timer Tracking
 
