4e4e9de3b428089a34e519b1fe49df8a393fb583
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-07-09 17:08:37 +0100

    Timer Consts Defined

--- typescript-pomodoro-timer.ts
+++ typescript-pomodoro-timer.ts
@@ -1,4 +1,6 @@
-//Define timer durations for work and break
+//Define timer durations for work and break (0. added for testing).
+const WORK_DURATION = 0.25; 
+const BREAK_DURATION = 0.5;
 
 //Start timer
 
