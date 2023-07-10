22aa7e6fae83e4a692019acffba48a7e6978e901
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-07-10 09:17:59 +0100

    setInterval bug fix

--- typescript-pomodoro-timer.ts
+++ typescript-pomodoro-timer.ts
@@ -7,28 +7,33 @@ function formatTime(minutes: number, seconds: number): string {
   return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
+
+
 //Timer Maths and output
 function startTimer(duration: number, message: string): void {
   console.log(`Starting ${message}`);
+
   //Multiply the 'duration' by 60 to get the duration in seconds.
   let remainingSeconds = duration * 60;
 
   //setInterval that prints to the log every second.
-  setInterval(() => {
+  const interval = setInterval(() => {
     const minutes = Math.floor(remainingSeconds / 60);
     const seconds = remainingSeconds % 60;
 
     console.log(formatTime(minutes, seconds));
 
+    //Print a completed message when finished.
+    if (remainingSeconds === 0) {
+      
+      //Clear the timer data
+      clearInterval(interval);
+      console.log(`${message} completed!`);
+    }
+
     //Reduce 1 second every second.
     remainingSeconds--;
-
-    //Print a completed message when finished.
-    if (remainingSeconds < 0) {
-      console.log(`${message} completed!`);
-      //Clear the timer data
-      clearInterval();
-    }
   }, 1000);
+}
   
 }
